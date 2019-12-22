96b9b2bdb1df81575b3ad508ed034443016b0451
stets <stetsblake@gmail.com>
2019-12-21 22:53:26 -0500

    vuejs shit

---
+++ job_board/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Jobs from '../views/Jobs.vue';
+
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    // component: () => import(/* webpackChunkName: "about" */ '../views/Jobs.vue'),
+    component: Jobs,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
